ef374ec116aa2e609476af9c1163aa78981b946b
tommaso122official <109802032+tommaso122official@users.noreply.github.com>
2024-04-16 16:23:15 +0200

    Update tracker2.ino

--- tracker2.ino
+++ tracker2.ino
@@ -1,14 +1,15 @@
 #include <Servo.h>
+
 #define wait 50
-int elab=0;
+
+int elab = 0;
 int array[8];
 Servo myservo1, myservo2;
 int analogPin = A2;
-/*int ser1 = 0;
-int ser2 = 0;*/
 int maxVoltage = 0;
 int optimalPos1 = 0;
 int optimalPos2 = 0;
+
 void setup() {
   myservo1.attach(9);
   myservo2.attach(8);
@@ -21,18 +22,30 @@ void loop() {
   myservo2.write(0);
   delay(100);  // Tempo utile per raggiungimento della posizione
   for (int pos1 = 0; pos1 <= 90; pos1++) {
-    int elab1=pos1*3;
+    int elab1 = pos1 * 3;
     myservo1.write(elab1);
     delay(wait);  // Tempo utile per raggiungimento della posizione
     for (int pos2 = 0; pos2 <= 30; pos2++) {
       elab++;
-      int elab2=pos2*3;
+      int elab2 = pos2 * 3;
       myservo2.write(elab2);
       delay(wait);  // Tempo utile per raggiungimento della posizione
       int voltage = analogRead(analogPin);
-      array[elab]=voltage;
-      if(elab==8)
-      elab=0;
+      array[elab % 8] = voltage; // Salva il valore nell'array, sovrascrivendo il valore più vecchio
+      bool Tolleranza = true;
+      for (int i = 1; i < 8; i++) {
+        if (abs(array[i] - array[i - 1]) > 10) { // Controlla la differenza tra i valori consecutivi
+          Tolleranza = false;
+          break;
+        }
+      }
+      if (Tolleranza) { // Se tutti i valori sono entro la tolleranza, considera questo come il massimo
+        if (voltage > maxVoltage) {
+          maxVoltage = voltage;
+          optimalPos1 = elab1;
+          optimalPos2 = elab2;
+        }
+      }
       Serial.println(" ");
       Serial.print("mV: ");
       Serial.print(voltage);
@@ -40,11 +53,6 @@ void loop() {
       Serial.print(elab1);
       Serial.print(" pos2: ");
       Serial.print(elab2);
-      if (voltage > maxVoltage) {
-        maxVoltage = voltage;
-        optimalPos1 = elab1;
-        optimalPos2 = elab2;
-      }
     }
   }
 
